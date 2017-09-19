e262eda86d127bf268b0efc22e6ef50190b8a7ba
Jonas Erixon <je222zj@student.lnu.se>
2017-09-19 14:04:53 +0200

    init

---
+++ 1dv607/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _1dv607
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Simpan!");
+        }
+    }
+}
